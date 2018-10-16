0f22803ab3ac10d6ab7b5f014974f57381644851
Надежда <nadia98@bigmir.net>
2018-10-16 22:16:59 +0300

    Добавлено уточнение моделей

--- GKS_kursov/MainWindow.xaml.cs
+++ GKS_kursov/MainWindow.xaml.cs
@@ -151,7 +151,7 @@ namespace GKS_kursov
             {
                 groups[iterator] = new List<int>();
 
-                int max = 0;
+                int maximum = 0;
                 List<int> MaxI = new List<int>();
                 List<int> MaxJ = new List<int>();
 
@@ -159,15 +159,15 @@ namespace GKS_kursov
                 {
                     for (int j = 0; j < n; j++)
                     {
-                        if (helpingMatrix[i, j] > max)
+                        if (helpingMatrix[i, j] > maximum)
                         {
                             MaxI.Clear();
                             MaxJ.Clear();
-                            max = helpingMatrix[i, j];
+                            maximum = helpingMatrix[i, j];
                             MaxI.Add(i);
                             MaxJ.Add(j);
                         }
-                        if ((helpingMatrix[i, j] == max) &&
+                        if ((helpingMatrix[i, j] == maximum) &&
                             ((MaxI.FindIndex(x => x == i) != -1) ||
                                 (MaxI.FindIndex(x => x == j) != -1) ||
                                 (MaxJ.FindIndex(x => x == i) != -1) ||
@@ -576,7 +576,7 @@ namespace GKS_kursov
                 arrayOfGroupModels[i] = listOfModels;
             }
 
-            PrintModel(arrayOfGroupModels);
+            PrintArrayOfModels(arrayOfGroupModels);
             #endregion
 
             #region Marging Models        
@@ -655,10 +655,71 @@ namespace GKS_kursov
 
             #endregion
 
-            tb.Text += "\nMerge Models\n";
-            PrintModel(arrayOfGroupModels);
+            tb.Text += "\nMerge Models:\n";
+            PrintArrayOfModels(arrayOfGroupModels);
 
             #endregion
+
+            #region Model Clarifying
+
+            List<List<string>> listOfAllModel = new List<List<string>>();
+            for (int i = 0; i < arrayOfGroupModels.Length; i++)
+                foreach (var listOfModel in arrayOfGroupModels[i])
+                    listOfAllModel.Add(listOfModel);
+
+            #region sortList
+            int max = 0;
+            for (int i = 0; i < listOfAllModel.Count - 1; i++)
+            {
+                max = i;
+                for (int j = i + 1; j < listOfAllModel.Count; j++)
+                    if (listOfAllModel[j].Count > listOfAllModel[max].Count)
+                        max = j;
+
+                if (max != i)
+                {
+                    var tmp = listOfAllModel[i];
+                    listOfAllModel[i] = listOfAllModel[max];
+                    listOfAllModel[max] = tmp;
+                }
+            }
+            #endregion
+
+            tb.Text += "\nAll Models:\n";
+            PrintModel(listOfAllModel);
+
+            List<List<string>> listOfClarifyingModel = new List<List<string>>();
+            for (int i = 0; i < listOfAllModel.Count; i++)
+                if (!FindInListOfAllModel(listOfAllModel[i], listOfClarifyingModel))
+                    listOfClarifyingModel.Add(listOfAllModel[i]);
+
+
+            tb.Text += "\nClarifying Models 1 part:\n";
+            PrintModel(listOfClarifyingModel);
+
+            for (int i = 0; i < listOfClarifyingModel.Count; i++)
+            {
+                for (int j = 0; j < listOfClarifyingModel[i].Count; j++)
+                {
+                    int index = 0;
+                    if ((index = FindOperationInListOfModel(listOfClarifyingModel[i][j], listOfClarifyingModel, i)) != -1)
+                    {
+                        if (listOfClarifyingModel[i].Count > listOfClarifyingModel[index].Count)
+                            listOfClarifyingModel[i].Remove(listOfClarifyingModel[i][j]);
+                        else
+                            listOfClarifyingModel[index].Remove(listOfClarifyingModel[i][j]);
+
+                        i = -1;
+                        break;
+                    }
+                }
+            }
+
+            tb.Text += "\nClarifying Models 2 part:\n";
+            PrintModel(listOfClarifyingModel);
+
+            #endregion
+
         }
 
         private void ButtonCreateGraf_Click(object sender, RoutedEventArgs e)
@@ -748,18 +809,24 @@ namespace GKS_kursov
             return uniqueOperationList;
         }
 
-        public void PrintModel(List<List<string>>[] arrayOfGroupModels)
+
+        public void PrintArrayOfModels(List<List<string>>[] arrayOfGroupModels)
         {
             for (int i = 0; i < arrayOfGroupModels.Length; i++)
             {
                 tb.Text += "\nModels of " + (i + 1) + " group\n";
-                for (int p = 0; p < arrayOfGroupModels[i].Count; p++)
-                {
-                    tb.Text += "Model " + (p + 1) + ": { ";
-                    foreach (var operation in arrayOfGroupModels[i][p])
-                        tb.Text += operation + " ";
-                    tb.Text += "}\n";
-                }
+                PrintModel(arrayOfGroupModels[i]);
+            }
+        }
+
+        public void PrintModel(List<List<string>> listOfModel)
+        {
+            for (int p = 0; p < listOfModel.Count; p++)
+            {
+                tb.Text += "Model " + (p + 1) + ": { ";
+                foreach (var operation in listOfModel[p])
+                    tb.Text += operation + " ";
+                tb.Text += "}\n";
             }
         }
 
@@ -868,5 +935,28 @@ namespace GKS_kursov
             }
             return false;
         }
+
+        public bool FindInListOfAllModel(List<string> currentModel, List<List<string>> listOfAllModels)
+        {
+            foreach (var model in listOfAllModels)
+            {
+                int cntSameOperation = 0;
+                foreach (var operation in currentModel)
+                    if (model.FindIndex(x => x == operation) != -1)
+                        cntSameOperation++;
+                if (cntSameOperation == currentModel.Count)
+                    return true;
+            }
+            return false;
+        }
+
+        public int FindOperationInListOfModel(string value, List<List<string>> listOfModels, int blockIndex)
+        {
+            for (int i = 0; i < listOfModels.Count; i++)
+                if (listOfModels[i].FindIndex(x => x == value) != -1 && blockIndex != i)
+                    return i;
+            return -1;
+        }
+
     }
 }
